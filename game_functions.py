d9b7cecb4a02499707e83cdff8de8d53e99cee71
FaulknerPearce <awesomeaustin1998@yahoo.com.au>
2023-10-06 15:31:34 -0600

    created functions for the game

---
+++ game_functions.py
@@ -0,0 +1,80 @@
+from game_classes import Character, Weapon
+import random
+
+# This fucntion creates an instance of the character and sword. 
+def create_player():
+    sword = Weapon(10, 20, 1, 0)
+    name = input("\nPlease enter you characters name: ")
+    created_player = Character(name, 50, "Human", 1, sword)
+    return created_player
+
+# This function will create an instance of a goblin that is the same level as the player. 
+def create_goblin():
+    sword = Weapon(10, 20, 1, 0)
+    created_goblin = Character("Goblin", 50, "Goblin", 1, sword)
+    return created_goblin
+
+# This function is called if block is False and will output different statements for the player or the opponent. 
+def output_if_not_block(_attacker, _opponent, _tactic, _damage):
+    if _attacker.race == "Human":
+        print(f"\nYour {_tactic} Attack was successful, you have reduced the {_opponent.name}'s Health by {_damage} points.") 
+    else:
+        print(f"\nYour block was unsuccessful. The {_attacker.name} reduced your Health by {_damage} points.")
+
+# This function is called if block is True and will output different statements for the player or the opponent.
+def output_if_block(_attacker, _opponent, _tactic):
+    if _attacker.race == "Human":
+        print(f"\nThe {_opponent.name} blocked your {_tactic} Attack.")
+    elif _opponent.race == "Human":
+        print(f"\nYou successfully blocked the {_attacker.name}'s {_tactic} Attack.")
+
+# This fucntion will use a int as a key for a dictionary and retrun the associated word. 
+def convert_int_to_word(player_choice):
+    attacks = {1: "Light", 2: "Heavy", 3: "Block", 4: "Parry"}
+    return attacks.get(int(player_choice), 3)
+
+def character_combat(_attacker, _opponent, _attack_tactic):
+    # If the opponents block is False, then reduce opponents health by the calculated damage of the attack (Light or Heavy). 
+    if not _opponent.block(_attacker, _attack_tactic):
+        damage_points = _attacker.attack(_opponent, _attack_tactic)
+        _attacker.weapon.add_xp(damage_points /2)
+        output_if_not_block(_attacker, _opponent, _attack_tactic, damage_points)
+    
+    # Otherwise no damage is dealt to the opponent and the attackers turn is over. 
+    else:
+       output_if_block(_attacker, _opponent, _attack_tactic)
+
+# This function is used to parry against an attack. 
+def parry(_defender, _opponent, defender_tactic):
+    if not _opponent.block(_defender, defender_tactic):
+        damage_to_enemy = _defender.parry(_opponent)
+        # Add weapon xp for successful hit 
+        _defender.weapon.add_xp(damage_to_enemy /2)
+        print(f"\nYour {defender_tactic} was succsessful, and you delt {damage_to_enemy} points of damage to the {_opponent.name}")
+    else:
+        damage_to_player = _opponent.parry(_defender)
+        print(f"\nYour {defender_tactic} was unsuccessful and the {_opponent.name} reduced your health by {damage_to_player} points.")
+
+# This fucntion will return the NPC's randomly selected attack choice, in the form of a string "Light" or "Heavy" attack.
+def enemy_random_attack():
+    num = random.randint(1,2)
+    word = convert_int_to_word(num)
+    return word
+
+
+
+
+
+    
+
+
+
+   
+
+
+
+   
+
+
+
+
